252b1122a0dbce7cdf5040d9bf0c4b9cd88a5334
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-12-04 10:56:43 +0200

    task 1

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,30 +1,29 @@
-import user from '../user.json'
-export default function Profile (){
+export default function Profile ({username,tag,location,avatar,stats}){
     return (
         <div className="profile">
   <div className="description">
     <img
-      src={user.avatar}
-      alt={user.username}
+      src={avatar}
+      alt={username}
       className="avatar"
     />
-    <p className="name">{user.username}</p>
-    <p className="tag">{user.tag}</p>
-    <p className="location">{user.location}</p>
+    <p className="name">{username}</p>
+    <p className="tag">{tag}</p>
+    <p className="location">{location}</p>
   </div>
 
   <ul className="stats">
-    <li>
+    <li className="stats__list">
       <span className="label">Followers </span>
-      <span className="quantity">{user.stats.followers}</span>
+      <span className="quantity">{stats.followers}</span>
     </li>
-    <li>
+    <li className="stats__list">
       <span className="label">Views </span>
-      <span className="quantity">{user.stats.views}</span>
+      <span className="quantity">{stats.views}</span>
     </li>
-    <li>
+    <li className="stats__list">
       <span className="label">Likes </span>
-      <span className="quantity">{user.stats.likes}</span>
+      <span className="quantity">{stats.likes}</span>
     </li>
   </ul>
 </div> 
